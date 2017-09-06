823269f1e529d8c8988ab0517f0b56e97da6efb3
DiceDiceBaby <jayejudd2@gmail.com>
2017-09-05 20:08:39 -0600

    Initial commit

---
+++ credentialsBot1.py
@@ -0,0 +1,6 @@
+
+keys = dict(
+consumer_key = "n4hr96w2wlyZHtbyQ0HBNfIZO",
+consumer_secret = "UPBRGYCCXwFQUeOQQ9yGxgYf0lOKBlp9YAcifQYuN4t7Lwl6Gi",
+access_token = "905206885346820099-jb8Edab5eLWnmjBcWDySpMsvh0G4Y1x",
+access_token_secret = "dunGRuH3OQk1toepturN96J0Q6sQe5T3HLjQSvnvSMmvU")
