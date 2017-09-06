38b6c317a2d961da203711f9b09a515ff982a4dc
DiceDiceBaby <jayejudd2@gmail.com>
2017-09-06 04:13:36 -0600

    Finished up likeBot
    Finished up likeBot

--- likeBot.py
+++ likeBot.py
@@ -10,3 +10,15 @@ ACCESS_TOKEN_SECRET = keys['access_token_secret']
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
+
+for tweet in tweepy.Cursor(api.search, q="#luxurybags").items():
+    try:
+
+        api.create_favorite(tweet.id)
+        print("I liked @" + tweet.user.screen_name)
+
+        sleep(300)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+        sleep(10)
